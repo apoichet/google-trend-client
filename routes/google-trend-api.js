2dded826e058157bf22d3f26ae570f442d0cd0c8
alexandre_poichet <apoichet@oui.sncf>
2018-11-11 20:04:33 +0100

    init

---
+++ routes/google-trend-api.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var googleTrends = require('google-trends-api');
+var moment = require('moment');
+const patternDate = 'YYYY-MM-DD';
+
+router.get('/interestOverTime', (req, res) => {
+    let requestObject = {
+        keyword: req.query['keyword'],
+        geo: req.query['geo']
+    };
+    if (req.query['startTime']) requestObject.startTime = moment(req.query['startTime'], patternDate).toDate();
+    if (req.query['endTime'])  requestObject.endTime = moment(req.query['endTime'], patternDate).toDate();
+
+    googleTrends.interestOverTime(requestObject)
+        .then(function(results){
+            res.send(results)
+        })
+        .catch(function(err){
+            console.error('Oh no there was an error', err);
+        });
+});
+
+module.exports = router;
