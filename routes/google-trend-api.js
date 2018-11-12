045fbc1392d0681195c2f085665dd538fac09794
Poichet Alexandre <alexandre_poichet@DX-8CN713054Y.groupevsc.com>
2018-11-12 18:18:00 +0100

    fix some issues

--- routes/google-trend-api.js
+++ routes/google-trend-api.js
@@ -5,14 +5,13 @@ var moment = require('moment');
 const patternDate = 'YYYY-MM-DD';
 
 router.get('/interestOverTime', (req, res) => {
+    console.log(req.query);
     let requestObject = {
-        keyword: req.query['keyword'],
+        keyword: req.query['keyword'].split(','),
         geo: req.query['geo'],
-        h1: req.query['h1'],
-        timezone: req.query['timezone'],
-        category: req.query['category'],
-        granularTimeResolution: req.query['granularTimeResolution']
+        h1: req.query['h1']
     };
+    console.log(requestObject);
     // Warning Invalid Date
     if (req.query['startTime']) requestObject.startTime = moment(req.query['startTime'], patternDate).toDate();
     if (req.query['endTime'])  requestObject.endTime = moment(req.query['endTime'], patternDate).toDate();
